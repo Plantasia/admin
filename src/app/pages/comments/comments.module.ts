cd403992dca3650e3d001a6bed2748ef9c4cfab4
marcos <marcos_v.23@hotmail.com>
2021-04-21 20:30:16 -0300

    *feature: custom modules

---
+++ src/app/pages/comments/comments.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommentsRoutingModule } from './comments-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommentsRoutingModule
+  ]
+})
+export class CommentsModule { }
