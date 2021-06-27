e87408f8b86d2a532aac7981301cd3acd3198d11
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-27 14:30:25 -0300

    lint and finish upload image category

--- src/app/pages/categories/categories-form/categories-form.component.ts
+++ src/app/pages/categories/categories-form/categories-form.component.ts
@@ -1,18 +1,28 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { CategoryModel } from './../../../models/category-model';
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { CategoryService } from 'src/app/services/category.service';
-import { catchError, switchMap } from 'rxjs/operators'
-import toastr from 'toastr';
-import { renderFlagCheckIfStmt } from '@angular/compiler/src/render3/view/template';
-import { ThisReceiver } from '@angular/compiler';
-import { throwError } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from "@angular/router";
+import { CategoryModel } from "./../../../models/category-model";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { CategoryService } from "src/app/services/category.service";
+import { catchError, switchMap } from "rxjs/operators";
+import toastr from "toastr";
+import { renderFlagCheckIfStmt } from "@angular/compiler/src/render3/view/template";
+import { ThisReceiver } from "@angular/compiler";
+import { throwError } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
 @Component({
-  selector: 'app-categories-form',
-  templateUrl: './categories-form.component.html',
-  styleUrls: ['./categories-form.component.css']
+  selector: "app-categories-form",
+  templateUrl: "./categories-form.component.html",
+  styleUrls: ["./categories-form.component.css"],
 })
 export class CategoriesFormComponent implements OnInit, AfterViewInit {
   currentAction: string;
@@ -22,7 +32,7 @@ export class CategoriesFormComponent implements OnInit, AfterViewInit {
   errorMessages: string[] = null;
   submittingForm: boolean = false;
   nameButton: string;
-  category: CategoryModel = new CategoryModel;
+  category: CategoryModel = new CategoryModel();
 
   constructor(
     private service: CategoryService,
@@ -30,11 +40,8 @@ export class CategoriesFormComponent implements OnInit, AfterViewInit {
     private router: Router,
     private formBuilder: FormBuilder,
     private cdRef: ChangeDetectorRef
-
-  ) { }
-  ngAfterViewInit(): void {
-
-  }
+  ) {}
+  ngAfterViewInit(): void {}
 
   ngOnInit() {
     this.setCurrentAction();
@@ -44,178 +51,140 @@ export class CategoriesFormComponent implements OnInit, AfterViewInit {
   }
 
   private buildCategoryForm() {
-
     this.categoryForm = new FormGroup({
-
       id: new FormControl({
-        value: null
+        value: null,
       }),
 
-      name: new FormControl('', [
+      name: new FormControl("", [Validators.required, Validators.minLength(5)]),
+
+      description: new FormControl("", [
         Validators.required,
-        Validators.minLength(5)
+        Validators.minLength(5),
       ]),
 
-      description: new FormControl('', [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
+      isActive: new FormControl(true, []),
 
-      isActive: new FormControl(true, [
+      authorEmail: new FormControl("", [Validators.minLength(10)]),
 
-      ]),
+      created_at: new FormControl("", [Validators.minLength(10)]),
 
-      authorEmail: new FormControl('', [
-        Validators.minLength(10)
-      ]),
+      updated_at: new FormControl("", [Validators.minLength(10)]),
 
-      created_at: new FormControl('', [
-        Validators.minLength(10)
-      ]),
+      deleted_at: new FormControl("", [Validators.minLength(10)]),
 
-      updated_at: new FormControl('', [
-        Validators.minLength(10)
-      ]),
-
-      deleted_at: new FormControl('', [
-        Validators.minLength(10)
-      ]),
-
-      imageStorage: new FormControl('', [])
-
-    })
+      imageStorage: new FormControl("", []),
+    });
   }
 
   private setCurrentAction() {
-    if (this.route.snapshot.url[0].path === 'new') {
+    if (this.route.snapshot.url[0].path === "new") {
       //[0] is to catch the 'new'
-      this.currentAction = 'new';
-
-    }
-    else {
-      this.currentAction = 'edit';
-
+      this.currentAction = "new";
+    } else {
+      this.currentAction = "edit";
     }
   }
 
   private setPageName() {
-    if (this.currentAction == 'new') {
-      this.pageName = "Nova Categoria"
-      this.nameButton = "Criar"
-      this.isCreating = true
-    }
-    else {
+    if (this.currentAction == "new") {
+      this.pageName = "Nova Categoria";
+      this.nameButton = "Criar";
+      this.isCreating = true;
+    } else {
       this.pageName = "Editar Categoria";
       this.isCreating = false;
-      this.nameButton = "Editar"
+      this.nameButton = "Editar";
     }
   }
 
   private handleFileInput(event) {
     if (event.target.files.length > 0) {
       const file: File = event.target.files[0];
-      this.categoryForm.get('imageStorage').patchValue({
-        imageStorage: file
-      }
-      );
+      this.categoryForm.get("imageStorage").patchValue({
+        imageStorage: file,
+      });
 
-      const formData = new FormData();
-
-      formData.append('file', file, file.name);
-      console.log("file")
-      console.log(file)
-
-      console.log("antes")
-      console.log(formData)
-      toastr.success(`Imagem ${file.name} atualizada!`)
-      /*
-      this.service.imageUpload(formData, this.category.id).subscribe(
-        (c) => {
-          console.log("upload")
-          console.log(c)
-        }
-      )*/
-
+      this.service
+        .imageUpload(file, this.category.id)
+        .pipe(
+          catchError((error) => {
+            console.log(error);
+            return undefined;
+          })
+        )
+        .subscribe((c) => {
+          console.log("upload");
+          console.log(c);
+        });
     }
   }
 
   private createCategory() {
-    const newCategory: CategoryModel =
-      Object.assign(new CategoryModel(), this.categoryForm.value)
+    const newCategory: CategoryModel = Object.assign(
+      new CategoryModel(),
+      this.categoryForm.value
+    );
 
-    this.service.create(newCategory).subscribe(
-      (c) => {
-        console.log(c)
-        toastr.success(`Categoria ${c.name} criada`)
-        this.submittingForm = false;
-      })
-
+    this.service.create(newCategory).subscribe((c) => {
+      console.log(c);
+      toastr.success(`Categoria ${c.name} criada`);
+      this.submittingForm = false;
+    });
   }
 
   private updateCategory() {
-    const category: CategoryModel =
-      Object.assign(new CategoryModel(), this.categoryForm.value)
+    const category: CategoryModel = Object.assign(
+      new CategoryModel(),
+      this.categoryForm.value
+    );
 
-    this.service.update(category.id, category).subscribe(
-
-      (c) => {
-        toastr.success(`Categoria ${c.name} atualizada!`)
-        this.submittingForm = false;
-        console.log("categoria atualizada!")
-        console.log(c);
-      }
-    )
+    this.service.update(category.id, category).subscribe((c) => {
+      toastr.success(`Categoria ${c.name} atualizada!`);
+      this.submittingForm = false;
+      console.log("categoria atualizada!");
+      console.log(c);
+    });
   }
 
   private changeStatus(category: CategoryModel) {
     if (category.isActive) {
       this.category.deleted_at = new Date().toLocaleDateString();
       this.category.isActive = false;
-    }
-
-    else {
+    } else {
       this.category.isActive = true;
       this.category.deleted_at = null;
     }
 
     this.categoryForm.patchValue(this.category);
-    this.categoryForm.get('isActive').setValue( this.category.isActive )
-    this.categoryForm.get('deleted_at').setValue(this.category.deleted_at)
-   
-    console.log("categoryForm")
-    console.log(this.categoryForm.value)
+    this.categoryForm.get("isActive").setValue(this.category.isActive);
+    this.categoryForm.get("deleted_at").setValue(this.category.deleted_at);
 
+    console.log("categoryForm");
+    console.log(this.categoryForm.value);
   }
 
   submit() {
     this.submittingForm = true;
 
-    if (this.currentAction === 'new') {
-      this.createCategory()
-    }
-
-    else {
-      this.updateCategory()
-
+    if (this.currentAction === "new") {
+      this.createCategory();
+    } else {
+      this.updateCategory();
     }
   }
 
   private loadCategory() {
-    if (this.currentAction === 'edit') {
-      this.route.paramMap.pipe(
-        switchMap(
-          params => this.service.getCategoryById(params.get('id'))
+    if (this.currentAction === "edit") {
+      this.route.paramMap
+        .pipe(
+          switchMap((params) => this.service.getCategoryById(params.get("id")))
         )
-      ).subscribe(
-        (category) => {
+        .subscribe((category) => {
           this.category = category;
           this.categoryForm.patchValue(category);
           console.log(category);
-        }
-      )
-
+        });
     }
-
-
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,57 +1,45 @@
-import { CategoryModel } from './../models/category-model';
-import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { CategoryModel } from "./../models/category-model";
+import { Injectable } from "@angular/core";
+import { HttpHeaders, HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoryService {
-
   constructor(private httpClient: HttpClient) {}
-  baseAdmin = 'http://localhost:3333/forum/categories/admin/list';
-  baseURL = 'http://localhost:3333/forum/categories/'
-  baseUpload = 'http://localhost:3333/forum/categories/image/'
-  
+  baseAdmin = "http://localhost:3333/forum/categories/admin/list";
+  baseURL = "http://localhost:3333/forum/categories/";
+  baseUpload = "http://localhost:3333/forum/categories/image/";
 
-
-  public getCategories():Observable<CategoryModel[]>{
-   return this.httpClient.get<CategoryModel[]>(this.baseAdmin);
+  public getCategories(): Observable<CategoryModel[]> {
+    return this.httpClient.get<CategoryModel[]>(this.baseAdmin);
   }
- 
-   public  getCategoryById(id:string):Observable<CategoryModel>{
-    var resp = this.httpClient.get<CategoryModel>(this.baseURL+'admin/'+id);
+
+  public getCategoryById(id: string): Observable<CategoryModel> {
+    var resp = this.httpClient.get<CategoryModel>(this.baseURL + "admin/" + id);
     return resp;
   }
 
-  public create(newCategory: CategoryModel):Observable<CategoryModel>{
+  public create(newCategory: CategoryModel): Observable<CategoryModel> {
     return this.httpClient.post<CategoryModel>(
-      this.baseURL+'admin', newCategory
-      );
+      this.baseURL + "admin",
+      newCategory
+    );
   }
 
-  public imageUpload(formData: FormData, id: string) {
-    console.log("entrei aqui")
-    console.log(formData)
-    return this.httpClient.post<any>(
-      this.baseUpload + id, formData, { headers: new HttpHeaders({ 'Content-Type': 'undefined' })  },
-    
-    )
+  public imageUpload(file: File, id: string): Observable<any> {
+    const formData = new FormData();
+    console.log(file);
+    formData.append("file", file, file.name);
+    return this.httpClient.post<any>(this.baseUpload + id, formData);
   }
 
-  public update(id, data: CategoryModel): Observable<CategoryModel>{
-    
-    return this.httpClient.patch<any>(
-      this.baseURL+id,
-      data
-      )
+  public update(id, data: CategoryModel): Observable<CategoryModel> {
+    return this.httpClient.patch<any>(this.baseURL + id, data);
   }
 
-  public delete(id):Observable<any> {
-    return this.httpClient.delete<any>(
-      this.baseURL+id
-    )
+  public delete(id): Observable<any> {
+    return this.httpClient.delete<any>(this.baseURL + id);
   }
-
-
 }
